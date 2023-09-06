2a33b6e84208244805a204d918bac9125374ae2a
JuanDBta <simplebetsas@gmail.com>
2023-09-06 14:47:10 -0500

    Add method to update likes

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,13 +2,13 @@ class Like < ApplicationRecord
     belongs_to :user
     belongs_to :post
 
-    after_create :increment_likescounter
+  after_create :increment_likescounter
   before_destroy :decrement_likescounter
   def increment_likescounter
-    user.increment!(:likescounter)
+    post.increment!(:likescounter)
   end
 
   def decrement_likescounter
-    user.decrement!(:likescounter)
+    post.decrement!(:likescounter)
   end
 end
