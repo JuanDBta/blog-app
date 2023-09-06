bacea4754ae50d878e89bd90d1fb06b4b4d6159c
JuanDBta <simplebetsas@gmail.com>
2023-09-06 14:50:07 -0500

    Fixed offenses

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,6 @@
 class Like < ApplicationRecord
-    belongs_to :user
-    belongs_to :post
+  belongs_to :user
+  belongs_to :post
 
   after_create :increment_likescounter
   before_destroy :decrement_likescounter
