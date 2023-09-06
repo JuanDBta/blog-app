c6864faa3b778e3aaae1872b58d4abf82c4014a5
JuanDBta <simplebetsas@gmail.com>
2023-09-05 20:19:49 -0500

    Fix typo errors

--- db/migrate/20230906002441_create_users.rb
+++ db/migrate/20230906002441_create_users.rb
@@ -5,7 +5,7 @@ class CreateUsers < ActiveRecord::Migration[7.0]
       t.string :photo
       t.text :bio
       t.timestamps null: false, default: -> { 'CURRENT_TIMESTAMP' }
-      t.integer :post_counter
+      t.integer :posts_counter
     end
 
     add_index :users, :name
