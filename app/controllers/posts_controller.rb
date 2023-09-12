f6177d72ccd1f619a141553bb64f0c1347610bfc
JuanDBta <simplebetsas@gmail.com>
2023-09-12 10:54:14 -0500

    Added logic to controllers

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,9 @@
+class PostsController < ApplicationController
+    def index
+      # Implement logic for the all posts by a given user page here
+    end
+  
+    def show
+      # Implement logic for the single post page here
+    end
+  end
