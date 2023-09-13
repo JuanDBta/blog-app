72bc950487006aee3ed6e958ff13ea9c76bde951
JuanDBta <simplebetsas@gmail.com>
2023-09-13 15:55:40 -0500

    Users tests done

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -12,22 +12,22 @@ RSpec.describe User, type: :request do
     end
     it 'response body includes correct placeholder text ' do
       get '/'
-      expect(response.body).to include('welcome to all users and thier respective number of posts')
+      expect(response.body).to include('BLOG APP: DISPLAY ALL USERS')
     end
   end
-  describe "'Get /show" do
+  describe "GET /show" do
     let(:user) { User.create(name: 'Helen', photo: 'https://unsplash/', bio: 'Programmer', posts_counter: 0) }
     it 'returns a successful response for specfic user' do
       get "/users/#{user.id}"
       expect(response).to have_http_status(:success)
     end
-    it 'renders the correct template for specfic user' do
+    it 'renders the correct template for specific user' do
       get "/users/#{user.id}"
       expect(response).to render_template(:show)
     end
-    it 'response body includes correct placeholder text for specfic user' do
+    it 'response body includes correct placeholder text for specific user' do
       get "/users/#{user.id}"
-      expect(response.body).to include('User Details')
+      expect(response.body).to include('Display Posts by User')
     end
   end
 end
