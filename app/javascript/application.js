c88f09fb292d5e92804519d0c80f104016b2effb
JuanDBta <simplebetsas@gmail.com>
2023-09-14 17:56:53 -0500

    Added functionality to See All Posts button

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,22 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+document.addEventListener("DOMContentLoaded", function() {
+    var button = document.getElementById("posts-button");
+  
+    // Función para mostrar todos los posts
+    function showAllPosts() {
+      var postsContainer = document.querySelector(".posts-container");
+      var postsContainerAll = document.querySelector(".posts-container-all");
+      postsContainer.style.display = "none";
+      postsContainerAll.style.display = "block";
+      button.style.display = "none"; // Oculta el botón después de mostrar todos los posts
+    }
+  
+    // Agrega un event listener al botón
+    button.addEventListener("click", function() {
+      showAllPosts();
+    });
+  });
+  
