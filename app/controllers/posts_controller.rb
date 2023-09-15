c4f0648f79a5e9d0a36a748077efb89ea2132fd2
JuanDBta <simplebetsas@gmail.com>
2023-09-15 12:09:23 -0500

    Created post in the form succesfully

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,14 +10,30 @@ class PostsController < ApplicationController
     @comments = @post.comments
   end
 
-  def create
-    @post = current_user.posts.build(post_params)
-    if @post.save
-      redirect_to @post, notice: "Post created !!!"
-    else
-      render "new"
+  def new
+    @post = Post.new
+    respond_to do |format|
+      format.html { render :new, locals: { post: @post } }
     end
   end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    respond_to do |format|
+      format.html do
+        if @post.save
+          flash[:success] = 'Post created !!!'
+          redirect_to user_posts_path(current_user)
+        else
+          # error message
+          flash.now[:error] = 'Error: Post not saved'
+          # render new
+          render :new, locals: { post: }
+        end
+      end
+    end
+  end
+
 
   private
 
