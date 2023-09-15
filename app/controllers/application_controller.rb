47a8f15f1305d13e3b20596a8a981c82872a4f7d
JuanDBta <simplebetsas@gmail.com>
2023-09-15 15:09:23 -0500

    Fixed offenses

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
-    def current_user
-        @current_user ||= User.first # This retrieves the first user from the database
-      end
-      helper_method :current_user
+  def current_user
+    @current_user ||= User.first # This retrieves the first user from the database
+  end
+  helper_method :current_user
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -34,7 +34,6 @@ class PostsController < ApplicationController
     end
   end
 
-
   private
 
   def set_user
