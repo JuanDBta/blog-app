0a5729f11acd70ffdf4d26ca01502e474c55217b
JuanDBta <simplebetsas@gmail.com>
2023-09-14 21:53:26 -0500

    Fix typo errors

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   root 'users#index'
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
-      resources :comments, only[:create, :new]
-      resources :likes, only[:create]
+      resources :comments, only: [:create, :new]
+      resources :likes, only: [:create]
   end
+end
 end
