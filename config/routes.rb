df6bd03f0900930d8661d39a362d03e943912ee0
JuanDBta <simplebetsas@gmail.com>
2023-09-22 16:17:37 -0500

    Modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
     get '/custom_sign_out' => 'devise/sessions#destroy', as: :custom_destroy_user_session
   end
   root 'users#index'
-  resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show new create] do
-      resources :comments, only: %i[create new]
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show, :new, :create, :destroy] do
+      resources :comments, only: [:create, :new, :destroy]
       resources :likes, only: [:create]
     end
   end
