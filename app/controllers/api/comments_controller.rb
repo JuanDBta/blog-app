1f42713b5300f8d26c6748759f295d5035e3cbb3
Gardimy Charles <gardimycharles889@gmail.com>
2023-09-23 06:25:22 -0400

    Update the api/comment_controller

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,27 +1,26 @@
 class Api::CommentsController < ApplicationController
-  class Api::CommentsController < ApplicationController
-    skip_before_action :verify_authenticity_token, only: [:create]
-    def index
-      @comments = Comment.where(post_id: params[:post_id])
-      render json: @comments, each_serializer: CommentSerializer
-    end
+  skip_before_action :verify_authenticity_token, only: [:create]
 
-    def create
-      @user = User.find(params[:user_id])
-      @post = @user.posts.find(params[:post_id])
-      @comment = @post.comments.build(comment_params)
-      @comment.author = @user
-      if @comment.save
-        render json: @comment, status: :created
-      else
-        render json: { errors: @comment.errors.full_messages }, status: :unprocessable_entity
-      end
-    end
+  def index
+    @comments = Comment.where(post_id: params[:post_id])
+    render json: @comments, each_serializer: CommentSerializer
+  end
 
-    private
-
-    def comment_params
-      params.require(:comment).permit(:text)
+  def create
+    @user = User.find(params[:user_id])
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.build(comment_params)
+    @comment.author = @user
+    if @comment.save
+      render json: @comment, status: :created
+    else
+      render json: { errors: @comment.errors.full_messages }, status: :unprocessable_entity
     end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
   end
 end
